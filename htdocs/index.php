45e3d3b1794560740cd0a43999e79ef111f8c291
Wouter Diesveld <wouter@querytechnology.com>
2016-03-03 15:26:29 +0100

    update intro page

--- htdocs/index.php
+++ htdocs/index.php
@@ -9,8 +9,7 @@ $publishUrl = 'https://' . $_SERVER["SERVER_NAME"] . '/api/';
 <p>Please follow these intructions to complete the setup:</p>
 <ul>
 	<li>Go to <a href="https://compiler1.tinyqueries.com/ide" target="_blank">TinyQueries</a></li>
-	<li>In the config section add this URL: <code><?php echo $publishUrl; ?></code> at publish settings</li>
-	<li>Click on the Setup button</li>
+	<li>Ensure that in the config section this URL <code><?php echo $publishUrl; ?></code> is set at publish settings</li>
 </ul>
 </body>
 </html>
